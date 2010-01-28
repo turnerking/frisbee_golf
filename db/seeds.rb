0c7f1d4b8fe0497137a52eef6c8147c54dbc7781
Turner King <turnerking@turner-kings-macbook.local>
2010-01-28 16:48:28 -0600

    Updated seed to include holes

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,13 @@
 user = User.create!(:name => "Admin", :login => "siteadmin", :email => "turneraking@gmail.com", :password => "P@55wOrD", :password_confirmation => "P@55wOrD")
 user.update_attribute(:admin, true)
 
-Course.create!(:name => "O'Brien Park",
+holes = (1..9).map{|i| Hole.create(:number => i, :par => ([1,2,9].include?(i) ? 4 : 3), :distance_in_ft => 200)}
+
+course = Course.create!(:name => "O'Brien Park",
                :address => "O'Brien Park, Downers Grove, IL 60516",
                :tee_type => "Gravel",
                :difficulty => 1,
                :tree_interference => 2,
                :hole_type => "Basket")
+               
+course.holes = holes
