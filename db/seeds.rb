fe8e66b60f5bc7bec121629142ee23b7052aa0ed
Turner King <turnerking@Admins-MacBook-Pro.local>
2010-05-04 15:24:00 -0500

    Updating seed data to include 2nd course and updated admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,49 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
 
-user = User.create!(:first_name => "Admin", :login => "siteadmin", :email => "turneraking@gmail.com", :password => "P@55wOrD", :password_confirmation => "P@55wOrD")
+user = User.create!(:first_name => "Admin", :last_name => "", :login => "siteadmin", :email => "turneraking@gmail.com", :password => "P@55wOrD", :password_confirmation => "P@55wOrD", :birthdate => "2010-01-13", :gender => "M", :state => "IL")
 user.update_attribute(:admin, true)
 
-holes = (1..9).map{|i| Hole.create(:number => i, :par => ([1,2,9].include?(i) ? 4 : 3), :distance_in_ft => 200)}
+#O'Brien Park, Downers Grove, IL
+obrien_park_holes = 
+  [ Hole.create(:number => 1, :par => 4, :distance_in_ft => 200),
+    Hole.create(:number => 2, :par => 4, :distance_in_ft => 200),
+    Hole.create(:number => 3, :par => 3, :distance_in_ft => 200),
+    Hole.create(:number => 4, :par => 3, :distance_in_ft => 200),
+    Hole.create(:number => 5, :par => 3, :distance_in_ft => 200),
+    Hole.create(:number => 6, :par => 3, :distance_in_ft => 200),
+    Hole.create(:number => 7, :par => 3, :distance_in_ft => 200),
+    Hole.create(:number => 8, :par => 3, :distance_in_ft => 200),
+    Hole.create(:number => 9, :par => 4, :distance_in_ft => 200)]
 
-course = Course.create!(:name => "O'Brien Park",
-               :address => "O'Brien Park, Downers Grove, IL 60516",
-               :tee_type => "Gravel",
-               :difficulty => 1,
-               :tree_interference => 2,
-               :hole_type => "Basket")
+obrien_park = 
+  Course.create!(:name => "O'Brien Park",
+                 :address => "O'Brien Park, Downers Grove, IL 60516",
+                 :tee_type => "Gravel",
+                 :difficulty => 1,
+                 :tree_interference => 2,
+                 :hole_type => "Basket")
                
-course.holes = holes
+obrien_park.holes = obrien_park_holes
+
+#Castaldo Park, Woodridge, IL
+castaldo_park_holes = 
+  [ Hole.create(:number => 1, :par => 4, :distance_in_ft => 280),
+    Hole.create(:number => 2, :par => 4, :distance_in_ft => 300),
+    Hole.create(:number => 3, :par => 5, :distance_in_ft => 405),
+    Hole.create(:number => 4, :par => 3, :distance_in_ft => 185),
+    Hole.create(:number => 5, :par => 3, :distance_in_ft => 235),
+    Hole.create(:number => 6, :par => 4, :distance_in_ft => 255),
+    Hole.create(:number => 7, :par => 4, :distance_in_ft => 275),
+    Hole.create(:number => 8, :par => 4, :distance_in_ft => 250),
+    Hole.create(:number => 9, :par => 4, :distance_in_ft => 330)]
+
+castaldo_park = 
+  Course.create!(:name => "Castaldo Park",
+                 :address => "3024 71st St., Woodridge, IL 60517",
+                 :tee_type => "Concrete",
+                 :difficulty => 3,
+                 :tree_interference => 3,
+                 :hole_type => "Basket")
+                 
+castaldo_park.holes = castaldo_park_holes
